eef1b0cb2e304e55fb739755a9a4c18b6c0486a0
Nemahs <robbymaura@gmail.com>
2019-04-07 08:47:28 -0400

    Began refactoring and commenting code

--- __init__.py
+++ __init__.py
@@ -3,8 +3,9 @@ import logging
 import os
 import re
 import sys
-
+sys.path.insert(1, "./submodules/dcs") # Add pydcs submodule to python load path
 import dcs
+import logging
 
 import ui.corruptedsavemenu
 import ui.mainmenu
@@ -12,6 +13,8 @@ import ui.newgamemenu
 import ui.window
 from game.game import Game
 from userdata import persistency, logging as logging_module
+
+#TODO: Change args to argparse
 
 assert len(sys.argv) >= 3, "__init__.py should be started with two mandatory arguments: %UserProfile% location and application version"
 
@@ -23,12 +26,24 @@ logging_module.setup_version_string(VERSION_STRING)
 logging.info("Using {} as userdata folder".format(persistency.base_path()))
 
 
+
+window = ui.window.Window()
+
 def proceed_to_main_menu(game: Game):
-    m = ui.mainmenu.MainMenu(w, None, game)
-    m.display()
+    """ Moves game to main menu
+
+        @param game  Game to move to main menu
+    """
+    menu = ui.mainmenu.MainMenu(window, None, game)
+    menu.display()
 
 
-def is_version_compatible(save_version):
+def is_version_compatible(save_version: str):
+    """ Check that the save version is compatible with the current version
+
+        @param save_version  String version to check fo compatibility
+        @return True if save_version is compatible with current version, False otherwise.
+    """
     current_version_components = re.split(r"[\._]", VERSION_STRING)
     save_version_components = re.split(r"[\._]", save_version)
 
@@ -47,18 +62,17 @@ def is_version_compatible(save_version):
     return False
 
 
-w = ui.window.Window()
+window = ui.window.Window()
 
 try:
     game = persistency.restore_game()
     if not game or not is_version_compatible(game.settings.version):
-        ui.newgamemenu.NewGameMenu(w, w.start_new_game).display()
+        ui.newgamemenu.NewGameMenu(window, window.start_new_game).display()
     else:
         game.settings.version = VERSION_STRING
         proceed_to_main_menu(game)
 except Exception as e:
     logging.exception(e)
-    ui.corruptedsavemenu.CorruptedSaveMenu(w).display()
+    ui.corruptedsavemenu.CorruptedSaveMenu(window).display()
 
-w.run()
-
+window.run()
--- theater/__init__.py
+++ theater/__init__.py
@@ -1,3 +1,6 @@
 from .controlpoint import *
 from .conflicttheater import *
 from .base import *
+
+import sys
+sys.path.insert(1, "../submodules/dcs/")
